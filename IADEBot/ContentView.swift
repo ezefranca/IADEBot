50b178c63b2a629443dc1a09526b3d61406ec15a
Ezequiel Santos <ezequiel.santos@miniclip.com>
2024-03-28 12:24:31 +0000

    Initial Commit

---
+++ IADEBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IADEBot
+//
+//  Created by Ezequiel dos Santos on 28/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IADEBot/IADEBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  IADEBotApp.swift
+//  IADEBot
+//
+//  Created by Ezequiel dos Santos on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct IADEBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
