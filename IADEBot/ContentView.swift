6590b4967eacb8b64cbcc879846b625918cb433b
Ezequiel Santos <ezequiel.santos@miniclip.com>
2024-04-23 09:35:07 +0100

    adding TTS

--- IADEBot/ContentView.swift
+++ IADEBot/ContentView.swift
@@ -6,19 +6,38 @@
 //
 
 import SwiftUI
+import SwiftUISpeechToText
 
 struct ContentView: View {
+    @StateObject var speechRecognizer = SpeechRecognizer(localeIdentifier: "pt-PT")
+    @State var title:String = "Start Recording"
+    @State var isRecording:Bool = false
+    @State var color:Color = .cyan
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("\((isRecording == false) ? "":((speechRecognizer.transcript == "") ? "Speak Now": speechRecognizer.transcript))")
+            Spacer()
+            Button("\(title)") {
+                isRecording.toggle()
+                if  isRecording {
+                    speechRecognizer.transcribe()
+                    title = "Stop Recording"
+                    color = .red
+                    //TTS_IOS().speak(speechRecognizer.transcript, voice: <#T##String!#>, volume: <#T##Float#>, pitch: <#T##Float#>, rate: <#T##Float#>, utterance_id: <#T##Int32#>, interrupt: <#T##Bool#>)
+                }else{
+                    speechRecognizer.stopTranscribing()
+                    title = "Start Recording"
+                    color = .cyan
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(color)
+            Spacer()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
